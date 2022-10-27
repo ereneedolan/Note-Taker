0619a919ef852579c27bbc51224280d73993d9c9
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-10-27 19:42:45 -0400

    created helper files and made routes for index and notes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const util = require('util');
-
-const uuid = require('./helpers/uuid');
+// const fs = require('fs');
+// const util = require('util');
+const { v4: uuidv4 } = require('uuid');
+const helpers = require('./utils/fs-helpers')
 
 const PORT = process.env.PORT||3001;
 
@@ -18,3 +18,13 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
--- utils/fs-helpers.js
+++ utils/fs-helpers.js
@@ -1,23 +1,14 @@
 // Promise version of fs.readFile
+const fs = require('fs');
+const util = require('util');
+
 const readFromFile = util.promisify(fs.readFile);
 
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
 const readAndAppend = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
@@ -29,3 +20,5 @@ const readAndAppend = (content, file) => {
     }
   });
 };
+
+module.exports = {readFromFile, writeToFile, readAndAppend}
